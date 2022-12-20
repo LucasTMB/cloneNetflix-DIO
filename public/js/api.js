ea02a0f4e6c16a1adced55bc7f25a1a01485c089
LucasTMB <lucas.tmb708@gmail.com>
2022-12-20 11:59:50 -0300

    first commit

---
+++ public/js/api.js
@@ -0,0 +1,4 @@
+let api_key = 'addef7a97fd22c0489cd610ee8ec9f10'
+let img_url = 'https://image.tmdb.org/t/p/w500'
+let genres_list_http = 'https://api.themoviedb.org/3/genre/movie/list?'
+let movie_genres_http = 'https://api.themoviedb.org/3/discover/movie?'
